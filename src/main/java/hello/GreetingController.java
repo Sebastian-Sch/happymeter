acdc837c2cfed88c5c2fdc4ffabc8680f834c82e
Sebastian Schmelcher <mail@sebastianschmelcher.de>
2017-10-22 13:16:20 +0200

    init

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,45 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    Map<String,Integer> dirtyPersistence = new HashMap<>();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="xx") String name, Model model) {
+        model.addAttribute("avg", getAvg());
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    private double getAvg() {
+        return dirtyPersistence.values().stream().mapToDouble(x -> x).summaryStatistics().getAverage();
+    }
+
+
+    @RequestMapping("/give")
+    public String greeting(@RequestParam String name, @RequestParam Integer value,  Model model) {
+        dirtyPersistence.put(name,value);
+        model.addAttribute("avg", getAvg());
+        model.addAttribute("name", name);
+        model.addAttribute("current",value);
+        return "greeting";
+    }
+
+    @RequestMapping("/reset")
+    public String reset(@RequestParam String name,  Model model) {
+        dirtyPersistence.remove(name);
+        model.addAttribute("avg", getAvg());
+        model.addAttribute("name", name);
+        model.addAttribute("current",0);
+        return "greeting";
+    }
+}
